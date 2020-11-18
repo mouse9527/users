b1cf305c917e200860c2b0a2876272a392c73901
xh.wang <xh.wang@datsch.com.cn>
2020-11-18 13:45:00 +0800

    Fixed build

---
+++ modules/iam.domain/src/test/java/com/mouse/users/iam/domain/TokenServiceTest.java
@@ -0,0 +1,19 @@
+package com.mouse.users.iam.domain;
+
+import com.mouse.framework.jwt.RSASigner;
+import org.junit.jupiter.api.Test;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class TokenServiceTest {
+    @Test
+    void should_be_able_to_allocate_token() throws NoSuchAlgorithmException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(1024);
+        KeyPair keyPair = generator.generateKeyPair();
+
+        RSASigner rsaSigner = new RSASigner(keyPair.getPrivate());
+    }
+}
