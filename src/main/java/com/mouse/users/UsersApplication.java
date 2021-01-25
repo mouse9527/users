b7b621811f598f2fcffa9cf8378b673293b51238
xh.wang <xh.wang@datsch.com.cn>
2021-01-25 17:27:26 +0800

    Added spring cloud config

--- src/main/java/com/mouse/users/UsersApplication.java
+++ src/main/java/com/mouse/users/UsersApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsersApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UsersApplication.class);
+        SpringApplication.run(UsersApplication.class, args);
     }
 }
