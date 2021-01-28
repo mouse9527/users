0346013ba2f41b61e9bfa9cfff75e97b6fae14d3
mouse9527 <m1689141525@gmail.com>
2021-01-28 11:39:40 +0800

    Refactored.

--- src/test/java/com/mouse/users/iam/test/IntegrationTest.java
+++ src/test/java/com/mouse/users/iam/test/IntegrationTest.java
@@ -3,10 +3,12 @@ package com.mouse.users.iam.test;
 import com.mouse.framework.test.EnableTestClient;
 import com.mouse.framework.test.ThreadSafeHeaderMockerGiven;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
 
 @Documented
+@ActiveProfiles("test")
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @EnableTestClient(ThreadSafeHeaderMockerGiven.class)
